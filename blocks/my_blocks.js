694450aafed008be659ce371539d86d9d6733917
Tao Liu <liu3tao@gmail.com>
2018-01-16 18:21:33 -0800

    Updated block definition with Python code generator.

--- blocks/my_blocks.js
+++ blocks/my_blocks.js
@@ -55,40 +55,29 @@ Blockly.defineBlocksWithJsonArray([{
 },
 {
   "type": "action_template_with_arg",
-  "message0": "actor 1: action 1 %1",
+  "message0": "actor 1: action 1 %1 %2 %3 %4",
   "args0": [
     {
+      "type": "input_dummy"
+    },
+    {
       "type": "input_value",
-      "name": "action_statment"
+      "name": "action_arg1"
+    },
+    {
+      "type": "input_value",
+      "name": "action_arg2"
+    },
+    {
+      "type": "input_value",
+      "name": "action_arg3"
     }
   ],
+  "inputsInline": true,
   "previousStatement": null,
   "nextStatement": null,
   "colour": 230,
   "tooltip": "Sample test action, with argument",
-  "helpUrl": ""
-},
-{
-  "type": "argument_template",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "input_value",
-      "name": "arg1"
-    },
-    {
-      "type": "input_value",
-      "name": "arg2"
-    },
-    {
-      "type": "input_value",
-      "name": "arg3"
-    }
-  ],
-  "inputsInline": true,
-  "output": null,
-  "colour": 230,
-  "tooltip": "Sample test action.",
   "helpUrl": ""
 },
 {
@@ -102,42 +91,33 @@ Blockly.defineBlocksWithJsonArray([{
 }]);
 
 // Code generator of the block.
-Blockly.JavaScript['profile_probability_block'] = function(block) {
+Blockly.Python['profile_probability_block'] = function(block) {
   var number_prob_val = block.getFieldValue('prob_val');
-  var statements_action_block = Blockly.JavaScript.statementToCode(block, 'action_block');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'profile_probability_block\n';
+  var statements_action_block = Blockly.Python.statementToCode(block, 'action_block');
+  var code = '- <Probability=' + number_prob_val + '>:\n';
+  code += statements_action_block;
   return code;
 };
 
-Blockly.JavaScript['profile_root_block'] = function(block) {
+Blockly.Python['profile_root_block'] = function(block) {
   var text_block_name = block.getFieldValue('block_name');
-  var statements_action_block = Blockly.JavaScript.statementToCode(block, 'action_block');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'profile_root_block\n';
+  var statements_action_block = Blockly.Python.statementToCode(block, 'action_block');
+  var code = text_block_name + ':\n';
+  code += statements_action_block;
   return code;
 };
 
-Blockly.JavaScript['action_template_with_arg'] = function(block) {
-  var value_action_statment = Blockly.JavaScript.valueToCode(
-      block, 'action_statment', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'action_template_with_arg\n';
+Blockly.Python['action_template_with_arg'] = function(block) {
+  var value_action_arg1 = Blockly.Python.valueToCode(block, 'action_arg1', Blockly.Python.ORDER_ATOMIC);
+  var value_action_arg2 = Blockly.Python.valueToCode(block, 'action_arg2', Blockly.Python.ORDER_ATOMIC);
+  var value_action_arg3 = Blockly.Python.valueToCode(block, 'action_arg3', Blockly.Python.ORDER_ATOMIC);
+  var code = '- Sample actor: sample action ' + '[' +
+              value_action_arg1 + ', ' + value_action_arg2 + ', ' + value_action_arg3 + ']' +
+              '\n';
   return code;
 };
 
-Blockly.JavaScript['argument_template'] = function(block) {
-  var value_arg1 = Blockly.JavaScript.valueToCode(block, 'arg1', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_arg2 = Blockly.JavaScript.valueToCode(block, 'arg2', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_arg3 = Blockly.JavaScript.valueToCode(block, 'arg3', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'argument_template';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['action_template_no_arg'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'action_template_no_arg\n';
+Blockly.Python['action_template_no_arg'] = function(block) {
+  var code = '- Sample actor: sample action\n';
   return code;
 };
